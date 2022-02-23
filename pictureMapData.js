c6e1f9e43c2dcd438170468c016a67619812461e
Jhh Lowengard <github@jhhl.net>
2022-02-23 16:57:18 -0500

    Initial check in  of resources
    including the old sonification webpage project.

---
+++ pictureMapData.js
@@ -0,0 +1,68 @@
+var pictureMap ={
+	"nickel":
+	{
+		"title":"In The Nickel Belt",
+		"artist":"Franklin Carmichael",
+		"date":"1928",
+		"medium":"Oil on Canvas",
+		"dimensions":"122.2 x 102.2 x 2.5 cm",
+		"id":"FAC 0108",
+		"collection":"Firestone Collection of Canadian Art, The Ottawa Art Gallery; Donated to the City of Ottawa by the Ontario Heritage Society",
+		"notes":"Individual descriptive areas",
+		"imageFile":"images/nba-1500.jpg",
+		"imageFileS":"images/nba-1500.svg",
+		"size":[1500,1264],
+		"svgFile":"images/nba-1500.svg",
+		"soundMap":
+		{
+			"#80f080":"sounds/cloud.mp3",
+			"#f08080":"sounds/sky.mp3",
+			"#8080f0":"sounds/ground.mp3"
+		}
+		
+	},
+		"nickela":
+	{
+		"title":"In The Nickel Belt",
+		"artist":"Franklin Carmichael",
+		"date":"1928",
+		"medium":"Oil on Canvas",
+		"dimensions":"122.2 x 102.2 x 2.5 cm",
+		"id":"FAC 0108",
+		"collection":"Firestone Collection of Canadian Art, The Ottawa Art Gallery; Donated to the City of Ottawa by the Ontario Heritage Society",
+		"notes":"shows active areas",
+		"imageFile":"images/nba-1500.svg",
+		"imageFileS":"images/nba-1500.svg",
+		"size":[1500,1264],
+		"svgFile":"images/nba-1500.svg",
+		"soundMap":
+		{
+			"#80f080":"sounds/cloud.mp3",
+			"#f08080":"sounds/sky.mp3",
+			"#8080f0":"sounds/ground.mp3"
+		}
+		
+	},
+		"nickelo":
+	{
+		"title":"In The Nickel Belt",
+		"artist":"Franklin Carmichael",
+		"date":"1928",
+		"medium":"Oil on Canvas",
+		"dimensions":"122.2 x 102.2 x 2.5 cm",
+		"id":"FAC 0108",
+		"collection":"Firestone Collection of Canadian Art, The Ottawa Art Gallery; Donated to the City of Ottawa by the Ontario Heritage Society",
+		"notes":"Jhhl: Overlain descriptive areas",
+		"imageFile":"images/nba-1500.jpg",
+		"imageFileS":"images/nba-1500.svg",
+		"size":[1500,1264],
+		"svgFile":"images/nbao-1500.svg",
+		"soundMap":
+		{
+			"#80f080":"sounds/cloud.mp3",
+			"#f08080":"sounds/sky.mp3",
+			"#8080f0":"sounds/ground.mp3"
+		}
+		
+	}
+};
