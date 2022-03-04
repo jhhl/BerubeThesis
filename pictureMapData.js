33120455d56f1b3a3b4cfea249201e2a3843c8be
Jhh Lowengard <github@jhhl.net>
2022-03-04 08:43:46 -0500

    integrated new paintings, desktop/laptop support is working

--- pictureMapData.js
+++ pictureMapData.js
@@ -10,16 +10,21 @@ var pictureMap ={
 		"collection":"Firestone Collection of Canadian Art, The Ottawa Art Gallery; Donated to the City of Ottawa by the Ontario Heritage Society",
 		"notes":"Individual descriptive areas",
 		"imageFile":"images/nba-1500.jpg",
-		"imageFileS":"images/nba-1500.svg",
 		"size":[1500,1264],
 		"svgFile":"images/nba-1500.svg",
 		"soundMap":
 		{
-			"#80f080":"sounds/cloud.mp3",
-			"#f08080":"sounds/sky.mp3",
-			"#8080f0":"sounds/ground.mp3"
+			"#80f080":"sounds/Shapes/2_clouds.mp3",
+			"#f08080":"sounds/Shapes/5_sky.mp3",
+			"#8080f0":"sounds/Shapes/4_valley.mp3"
+		},
+		"descriptionMap":
+		{
+			"#80f080":"sounds/Descriptions/2_clouds.mp3",
+			"#f08080":"sounds/Descriptions/5_sky.mp3",
+			"#8080f0":"sounds/Descriptions/4_valley.mp3"
 		}
-		
+
 	},
 		"nickela":
 	{
@@ -37,11 +42,16 @@ var pictureMap ={
 		"svgFile":"images/nba-1500.svg",
 		"soundMap":
 		{
-			"#80f080":"sounds/cloud.mp3",
-			"#f08080":"sounds/sky.mp3",
-			"#8080f0":"sounds/ground.mp3"
+			"#80f080":"sounds/Shapes/2_clouds.mp3",
+			"#f08080":"sounds/Shapes/5_sky.mp3",
+			"#8080f0":"sounds/Shapes/4_valley.mp3"
+		},
+		"descriptionMap":
+		{
+			"#80f080":"sounds/Descriptions/2_clouds.mp3",
+			"#f08080":"sounds/Descriptions/5_sky.mp3",
+			"#8080f0":"sounds/Descriptions/4_valley.mp3"
 		}
-		
 	},
 		"nickelo":
 	{
@@ -54,14 +64,102 @@ var pictureMap ={
 		"collection":"Firestone Collection of Canadian Art, The Ottawa Art Gallery; Donated to the City of Ottawa by the Ontario Heritage Society",
 		"notes":"Jhhl: Overlain descriptive areas",
 		"imageFile":"images/nba-1500.jpg",
-		"imageFileS":"images/nba-1500.svg",
 		"size":[1500,1264],
 		"svgFile":"images/nbao-1500.svg",
 		"soundMap":
 		{
-			"#80f080":"sounds/cloud.mp3",
-			"#f08080":"sounds/sky.mp3",
-			"#8080f0":"sounds/ground.mp3"
+			"#80f080":"sounds/Shapes/2_clouds.mp3",
+			"#f08080":"sounds/Shapes/5_sky.mp3",
+			"#8080f0":"sounds/Shapes/4_valley.mp3"
+		},
+		"descriptionMap":
+		{
+			"#80f080":"sounds/Descriptions/2_clouds.mp3",
+			"#f08080":"sounds/Descriptions/5_sky.mp3",
+			"#8080f0":"sounds/Descriptions/4_valley.mp3"
+		}
+		
+	},
+"Painting1":
+	{
+		"title":"In The Nickel Belt",
+		"artist":"Franklin Carmichael",
+		"date":"1928",
+		"medium":"Oil on Canvas",
+		"dimensions":"122.2 x 102.2 x 2.5 cm",
+		"id":"FAC 0108",
+		"collection":"Firestone Collection of Canadian Art, The Ottawa Art Gallery; Donated to the City of Ottawa by the Ontario Heritage Society",
+		"notes":"Jhhl: Overlain descriptive areas",
+		"imageFile":"images/Painting1.jpg",
+		"size":[1057,880],
+		"svgFile":"images/Painting1.svg",
+		"soundMap":
+		{
+			"#808080":"sounds/Shapes/1_smoke_wind.mp3",
+			"#C0C0C0":"sounds/Shapes/2_clouds.mp3",
+			"#404080":"sounds/Shapes/3_rain.mp3",
+			"#804000":"sounds/Shapes/4_valleys.mp3",
+			"#80C0F0":"sounds/Shapes/5_sky.mp3",
+			"#C08080":"sounds/Shapes/6_moon.mp3",
+			"#80F080":"sounds/Shapes/7_boats.mp3",
+			"#F080F0":"sounds/Shapes/8_habitations.mp3",
+			"#F08000":"sounds/Shapes/9_docks.mp3",
+			"#0040C0":"sounds/Shapes/10_water.mp3"
+		},
+		"descriptionMap":
+		{
+			"#808080":"sounds/Descriptions/1_smoke_wind.mp3",
+			"#C0C0C0":"sounds/Descriptions/2_clouds.mp3",
+			"#404080":"sounds/Descriptions/3_rain.mp3",
+			"#804000":"sounds/Descriptions/4_valleys.mp3",
+			"#80C0F0":"sounds/Descriptions/5_sky.mp3",
+			"#C08080":"sounds/Descriptions/6_moon.mp3",
+			"#80F080":"sounds/Descriptions/7_boats.mp3",
+			"#F080F0":"sounds/Descriptions/8_habitations.mp3",
+			"#F08000":"sounds/Descriptions/9_docks.mp3",
+			"#0040C0":"sounds/Descriptions/10_water.mp3"
+		}
+		
+	},
+
+"Painting2":
+	{
+		"title":"Outport Icon #2",
+		"artist":"Duncan De Kergommeaux",
+		"date":"2001-2002",
+		"medium":"Oil on canvas",
+		"dimensions":"106.7 x 106.7 x 3.2 cm",
+		"id":"2020.01.09",
+		"collection":"Collection of The Ottawa Art Gallery / Collection de La Galerie d'art d'Ottawa: Gift of the artist / Don de l'artiste ",
+		"notes":"Jhhl: Overlain descriptive areas",
+		"imageFile":"images/Painting2.jpg",
+		"size":[1080,1080],
+		"svgFile":"images/Painting2.svg",
+		"soundMap":
+		{
+			"#808080":"sounds/Shapes/1_smoke_wind.mp3",
+			"#C0C0C0":"sounds/Shapes/2_clouds.mp3",
+			"#404080":"sounds/Shapes/3_rain.mp3",
+			"#804000":"sounds/Shapes/4_valleys.mp3",
+			"#80C0F0":"sounds/Shapes/5_sky.mp3",
+			"#C08080":"sounds/Shapes/6_moon.mp3",
+			"#80F080":"sounds/Shapes/7_boats.mp3",
+			"#F080F0":"sounds/Shapes/8_habitations.mp3",
+			"#F08000":"sounds/Shapes/9_docks.mp3",
+			"#0040C0":"sounds/Shapes/10_water.mp3"
+		},
+		"descriptionMap":
+		{
+			"#808080":"sounds/Descriptions/1_smoke_wind.mp3",
+			"#C0C0C0":"sounds/Descriptions/2_clouds.mp3",
+			"#404080":"sounds/Descriptions/3_rain.mp3",
+			"#804000":"sounds/Descriptions/4_valleys.mp3",
+			"#80C0F0":"sounds/Descriptions/5_sky.mp3",
+			"#C08080":"sounds/Descriptions/6_moon.mp3",
+			"#80F080":"sounds/Descriptions/7_boats.mp3",
+			"#F080F0":"sounds/Descriptions/8_habitations.mp3",
+			"#F08000":"sounds/Descriptions/9_docks.mp3",
+			"#0040C0":"sounds/Descriptions/10_water.mp3"
 		}
 		
 	}
